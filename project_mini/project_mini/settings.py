1bf8988a6123d7557956fe2180d3f1e7905b6f18
rgiri9041 <rgiri9041@gmail.com>
2022-11-08 22:23:06 +0545

    mysql configure/setup and admin migration

--- project_mini/project_mini/settings.py
+++ project_mini/project_mini/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'project_mini.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
+   # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mini_ims',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
     }
 }
 
